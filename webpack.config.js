6c8a15ae7ef2900e71f0257f4963a47710a87f05
Pokdee <loselife61@gmail.com>
2023-05-19 00:08:44 +0700

    Revert "Basic structure"
    This reverts commit c2febe9c5c8c1db8f4992da30782f20f7dde284a.

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,4 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 9000,
-  },
 };
